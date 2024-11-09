c764f0d9750f965f860f9c1e523305677cd83a88
Maya Eid <mayabaseleid@gmail.com>
2024-11-09 14:55:07 +0200

    Scrape and add each movie info to array of objects

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -6,6 +6,17 @@ window.onload = () => {
     let allDurations = document.querySelectorAll('.card-screening__right .mb-lg-4 div.ft-secondary > span > span');
     let allGenres = document.querySelectorAll('.card-screening__right .mb-lg-4 div.ft-secondary > span');
     
+    allNames.forEach((link, index)=>{
+        let movie = {
+            name: link.textContent.trim(),
+            image: allImages[index] ? allImages[index].src : "N/A",
+            duration: allDurations[index] ? allDurations[index].textContent.trim() : "N/A",
+            genre: allGenres[index] ? allGenres[index].childNodes[0].textContent.trim() : "N/A"
+        }
+        movies.push(movie);
+    });
+
+console.log(movies);
 
 
     
