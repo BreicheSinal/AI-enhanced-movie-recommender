e697279e53a309cf27861490b6e3d0764b2cc5c4
Maya Eid <mayabaseleid@gmail.com>
2024-11-09 14:54:21 +0200

    Select movie data elements: names, durations, genres, and images

---
+++ scripts/scrape.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+
+    let movies = [];
+    let allImages=document.querySelectorAll('.card-screening__img > img');
+    let allNames = document.querySelectorAll('.card-screening__right .mb-lg-4 p.h3 a');
+    let allDurations = document.querySelectorAll('.card-screening__right .mb-lg-4 div.ft-secondary > span > span');
+    let allGenres = document.querySelectorAll('.card-screening__right .mb-lg-4 div.ft-secondary > span');
+    
+
+
+    
+}
