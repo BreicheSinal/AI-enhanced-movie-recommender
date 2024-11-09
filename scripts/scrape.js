d091615968bb2ac4b96a61bbbd5429ac11ce8191
Maya Eid <mayabaseleid@gmail.com>
2024-11-09 15:08:52 +0200

    Detect New label for each movie card

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -5,14 +5,16 @@ window.onload = () => {
     let allNames = document.querySelectorAll('.card-screening__right .mb-lg-4 p.h3 a');
     let allDurations = document.querySelectorAll('.card-screening__right .mb-lg-4 div.ft-secondary > span > span');
     let allGenres = document.querySelectorAll('.card-screening__right .mb-lg-4 div.ft-secondary > span');
-    
-    allNames.forEach((link, index)=>{
+    let allCardWrappers = document.querySelectorAll('.card-screening__right');
+
+    allCardWrappers.forEach((cardWrapper, index) => {
         let movie = {
-            name: link.textContent.trim(),
+            name: allNames[index] ? allNames[index].innerText.trim() : "N/A",
+            duration: allDurations[index] ? allDurations[index].innerText.trim() : "N/A",
+            genre: allGenres[index] ? allGenres[index].innerText.trim() : "N/A",
             image: allImages[index] ? allImages[index].src : "N/A",
-            duration: allDurations[index] ? allDurations[index].textContent.trim() : "N/A",
-            genre: allGenres[index] ? allGenres[index].childNodes[0].textContent.trim() : "N/A"
-        }
+            isNew: cardWrapper.querySelector('.label--red') ? true : false
+        };
         movies.push(movie);
     });
 
