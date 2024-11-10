4fb885f558598b28865a99d2c6a778fd2ec74305
Maya Eid <mayabaseleid@gmail.com>
2024-11-10 10:54:52 +0200

    Insert movie_id to bookmark table with user_id as 1 (before adding user_id to local storage)

--- server/bookmark_movies.php
+++ server/bookmark_movies.php
@@ -14,7 +14,22 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 if (isset($data['movie_id'])) {
     $movie_id = $data['movie_id'];
-    echo $movie_id; 
+    $user_id = 1;
+
+    $addBookmarkedMovie = 'INSERT INTO bookmark (user_id, movie_id) VALUES (?,?)'; 
+    if($query = $conn->prepare($addBookmarkedMovie)){
+        $query->bind_param('ii',$user_id,$movie_id);
+
+        if($query->execute()){
+            echo "Movie bookmarked";
+        }else{
+            echo "Didn't bookmark";
+        }
+        $query->close();
+    }else{
+        echo "Failed to prepare query";
+    }
+
 } else {
     echo "Movie ID not provided.";
 }
