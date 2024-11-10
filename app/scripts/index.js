885ff3ffb4b3354bc7c0676a7032c5dacf6f7cb7
BreicheSinal <sinalbreiche@gmail.com>
2024-11-10 03:50:31 +0200

    Added blueprint for bot interface with API integration

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,3 @@
-const botBttn = document.getElementById("botBttn");
-
 botBttn.addEventListener("click", function () {
   const chatbox = document.getElementById("chatbox");
   console.log("im here");
@@ -8,3 +6,22 @@ botBttn.addEventListener("click", function () {
       ? "block"
       : "none";
 });
+
+const apiURL = "https://api.openai.com/v1/chat/completions";
+//const apiKey =
+//  "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";
+
+const apiKey = "";
+
+//  adding event handler "onclick"
+sendBtn.onclick = async function () {};
+
+function addUserMessage(msg) {}
+
+function addChatbotPlaceholder() {}
+
+async function fetchData(userMsg) {}
+
+function updateChatbotResponse(msgContent) {}
+
+function showError(errorMsg) {}
