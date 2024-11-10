c446c42b3c5749c35feecfdee8483ef1497e2130
Maya Eid <mayabaseleid@gmail.com>
2024-11-10 08:35:10 +0200

    Display retrieved data in html for testing

--- app/scripts/get_movies.js
+++ app/scripts/get_movies.js
@@ -1,5 +1,18 @@
 fetch('http://localhost/AI-enhanced-movie-recommender-main/AI-enhanced-movie-recommender/server/get_movies.php')
     .then(response => response.json())
     .then(movies => {
-        console.log(movies);
+        const moviesContainer = document.getElementById("movies-container");
+
+        movies.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.innerHTML=`
+            <p>${movie.title}</p>
+            <p>${movie.description}</p>
+            <p>${movie.release_year}</p>
+            <p>${movie.genre}</p>
+            <p>${movie.duration}</p>
+            `
+            moviesContainer.appendChild(movieElement);
+        });
     });
+    
