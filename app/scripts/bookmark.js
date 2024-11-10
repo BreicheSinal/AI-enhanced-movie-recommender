58810f1d711562592822ebfb9f0cdf8fe619cff5
Maya Eid <mayabaseleid@gmail.com>
2024-11-10 14:18:35 +0200

    Add bookmark color based on status

--- app/scripts/bookmark.js
+++ app/scripts/bookmark.js
@@ -1,7 +1,6 @@
 const user_id = localStorage.getItem("user_id");
 
-
-function checkBookmarkStatus(movie){
+function checkBookmarkStatus(bookmark,movie){
     fetch('http://localhost/AI-enhanced-movie-recommender-main/AI-enhanced-movie-recommender/server/check_bookmark.php',{
         method:'POST',
         headers:{
@@ -16,15 +15,17 @@ function checkBookmarkStatus(movie){
     .then(data=>{
         if(data.bookmarked){
             //if movie is bookmarked, set the btn as bookmarked
-            setBookmarked();
+            setBookmarked(bookmark);
         }else{
-            setNotBookmarked();
+            setNotBookmarked(bookmark);
         }
     })
     .catch(error => console.log(error));
 }
 
 function toggleBookmark(bookmark, movie){
+    checkBookmarkStatus(bookmark,movie);
+
     if(bookmark.classList.contains('clicked')){
         //if already bookmarked, remove it 
         removeBookmark(bookmark, movie);
@@ -85,4 +86,24 @@ function removeBookmark(bookmark, movie){
         }        
     })
     .catch(error => console.log('Error: ', error)); 
+}
+
+//set button as clicked
+function setBookmarked(bookmark) {
+    bookmark.innerHTML = `
+        <svg class='clicked-bookmark' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#FFCC00" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark">
+            <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/>
+        </svg>
+    `;
+    bookmark.classList.add('clicked'); // Mark as clicked
+}
+
+//set button as not clicked
+function setNotBookmarked() {
+    bookmark.innerHTML = `
+        <svg class='not-clicked-bookmark' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark">
+            <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/>
+        </svg>
+    `;
+    bookmark.classList.remove('clicked'); // Remove clicked class
 }
