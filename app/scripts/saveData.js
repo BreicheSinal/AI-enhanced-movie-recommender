390f88db9b01e085347c3bfa482c38a112aef358
BreicheSinal <sinalbreiche@gmail.com>
2024-11-12 03:29:51 +0200

    Refactored saveData.js - made function of IIFE Structure JS <>

--- app/scripts/saveData.js
+++ app/scripts/saveData.js
@@ -1,17 +1,21 @@
-const BASE_URL =
-  "http://localhost/AI-enhanced-movie-recommender-main/AI-enhanced-movie-recommender";
+//  function is defined and immediately called - IIFE STRUCTURE
+(function fetchAndPostData() {
+  const BASE_URL =
+    "http://localhost/AI-enhanced-movie-recommender-main/AI-enhanced-movie-recommender";
 
-fetch(`${BASE_URL}/app/assets/scraped/scrapedData.json`)
-  .then((moviesData) => {
-    axios
-      .post(`${BASE_URL}/server/saveData.php`, moviesData)
-      .then((response) => {
-        console.log("Response from server:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  })
-  .catch((error) => {
-    console.error("Error fetching JSON file:", error);
-  });
+  fetch(`${BASE_URL}/app/assets/scraped/scrapedData.json`)
+    .then((response) => response.json())
+    .then((moviesData) => {
+      axios
+        .post(`${BASE_URL}/server/saveData.php`, moviesData)
+        .then((response) => {
+          console.log("Response from server:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error fetching JSON file:", error);
+    });
+})();
