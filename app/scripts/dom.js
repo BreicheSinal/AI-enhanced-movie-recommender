cc6175ec6d45a13e179749331195ada8d6722dae
BreicheSinal <sinalbreiche@gmail.com>
2024-11-14 03:17:03 +0200

    Added an event listener on get started button

--- app/scripts/dom.js
+++ app/scripts/dom.js
@@ -4,6 +4,7 @@
 const botBttn = document.getElementById("botBttn");
 const sendBtn = document.getElementById("sendButton");
 const sigIN = document.getElementById("signin-btn");
+const getStarted = document.getElementById("getStarted");
 
 const messageBox = document.querySelector(".message-box");
 const messageBar = document.querySelector(".bar-wrapper input");
